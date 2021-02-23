ad42cd7556c44431deabc16fafb0432dbd69f4c1
Quentin Pla <quentin-pla@hotmail.fr>
2021-02-23 17:26:04 +0100

    Implémentation calendrier

--- Test_technique/ClientApp/src/components/Home.tsx
+++ Test_technique/ClientApp/src/components/Home.tsx
@@ -1,26 +1,128 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { EventApi, DateSelectArg, EventClickArg } from "@fullcalendar/react";
+import {Calendar} from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
 
-export class Home extends Component {
-  static displayName = Home.name;
+type Props = {}
+type State = {
+    currentEvents: EventApi[],
+    todayStr: string,
+    isError: boolean,
+    eventCount: number,
+    calendar: null|Calendar,
+}
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+export class Home extends Component<Props, State> {
+    static displayName = Home.name;
+    
+    private readonly calendarRef: React.RefObject<HTMLDivElement>;
+
+    constructor(props: Props) {
+        super(props);
+        this.calendarRef = React.createRef();
+    }
+    
+    state: State = {
+        currentEvents: [],
+        todayStr: new Date().toISOString().replace(/T.*$/, ''),
+        isError: false,
+        eventCount: 0,
+        calendar: null
+    };
+
+    componentDidMount() {
+        let calendar = new Calendar(this.calendarRef.current as HTMLElement, {
+            plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin],
+            headerToolbar: false,
+            initialView: 'dayGridMonth',
+            editable: true,
+            selectable: true,
+            selectMirror: true,
+            events: this.handleInitEvent,
+            select: this.handleDateSelect,
+            eventClick: this.handleEventClick,
+            eventsSet: this.handleEventsUpdate,
+        });
+        calendar.render();
+        this.setState({calendar});
+    }
+
+    handleInitEvent = (fetchInfo: object, successCallback: Function, failureCallback: Function) => {
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        };
+        fetch('https://localhost:5001/api/calendar/events', requestOptions).then(res => {
+            if (res.status !== 200) {
+                this.setState({isError: true});
+                failureCallback();
+            } else {
+                res.json().then(json => {
+                    successCallback(json);
+                });
+            }
+        });
+    }
+
+    handleDateSelect = (selectInfo: DateSelectArg) => {
+        let title = prompt("Saisir le titre de l'évènement");
+        const calendar = selectInfo.view.calendar;
+        calendar.unselect();
+        if (title) {
+            let eventData = {
+                title,
+                start: selectInfo.start,
+                end: selectInfo.end,
+                allDay: selectInfo.allDay
+            };
+            this.uploadEvent(eventData);
+        }
+    }
+    
+    uploadEvent(eventData: object) {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(eventData)
+        };
+        fetch('https://localhost:5001/api/calendar/events', requestOptions).then(response => {
+            if (response.status !== 200)
+                this.setState({isError: true});
+            else
+                this.state.calendar!.addEvent([eventData]);
+        });
+    }
+
+    createEventId = () => {
+        this.setState((state) => ({eventCount: state.eventCount + 1}));
+        return String(this.state.eventCount);
+    }
+
+    // renderEventContent = (eventInfo: any) => {
+    //     console.log(eventInfo);
+    //     return (
+    //         <>
+    //             <b>{eventInfo.timeText}</b>
+    //             <i>{eventInfo.event.title}</i>
+    //         </>
+    //     )
+    // }
+
+    handleEventClick = (clickInfo: EventClickArg) => {
+       //
+    }
+
+    handleEventsUpdate = (events: EventApi[]) => {
+        this.setState({currentEvents: events});
+    }
+
+    render() {
+        return (
+            <div>
+                <div ref={this.calendarRef}/>
+            </div>
+        );
+    }
 }
